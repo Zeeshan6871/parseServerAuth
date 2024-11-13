f4a671e69ec19b5ca68d9303d70844ca292fddbe
Zeeshan6871 <zeeshanahmad6871@gmail.com>
2024-11-13 15:48:22 +0530

    push

--- frontend/src/services/parse.services.js
+++ frontend/src/services/parse.services.js
@@ -2,7 +2,7 @@ import Parse from 'parse';
 
 // Initialize Parse with your app's configuration
 Parse.initialize(process.env.REACT_APP_PARSE_APP_ID || "myAppId");
-Parse.serverURL =  'http://localhost:1337/parse';
+Parse.serverURL = process.env.REACT_APP_SERVER_URL || 'http://localhost:1337/parse';
 
 // console.log(process.env.REACT_APP_PARSE_APP_ID,process.env.REACT_APP_SERVER_URL );
 
